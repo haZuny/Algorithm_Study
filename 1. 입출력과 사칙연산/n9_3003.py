0c0cf67070b1648f1afe39dd6638d1119761b9df
hajunKwon <gkwns5791@naver.com>
2022-12-26 21:18:58 +0900

    1단계 완료

---
+++ 1. 입출력과 사칙연산/n9_3003.py
@@ -0,0 +1,8 @@
+# map: 리스트의 모든 요소를 주어진 함수와 매핑하여 반환
+k, q, l, b, n, p = map(int, input().split())
+print(str(1 - k) + " ", end='')
+print(str(1 - q) + " ", end='')
+print(str(2 - l) + " ", end='')
+print(str(2 - b) + " ", end='')
+print(str(2 - n) + " ", end='')
+print(str(8 - p) + " ", end='')
