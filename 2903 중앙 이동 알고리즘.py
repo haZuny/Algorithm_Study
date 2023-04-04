a4aa25ef453243b6a04a3fa9a7096cfa13486901
hajunKwon <gkwns5791@naver.com>
2023-04-04 13:46:48 +0900

    2903

--- 2903 중앙 이동 알고리즘.py
+++ 2903 중앙 이동 알고리즘.py
@@ -1,11 +1,21 @@
-0   0
-1   1
-2   3
-3   7
+import sys
 
-# 각 모서리: 1개
-# 모서리를 제외한 한 변의 점: 2개 -> 2n 
-# 그 외의 점: 3개
+n = int(sys.stdin.readline())
 
+sidePoint = 1
 
+# 양 끝을 제외한 한 변의 점의 수 구하기
+for i in range(1, n):
+    sidePoint = sidePoint * 2 + 1
 
+# 전체 사각형 수
+numSquare = (2**n)**2
+
+# 중복 포함한 점의 수
+allPoint = numSquare * 4
+
+# 중복 제거 점의 수
+    # 1. 각 변의 점은 2개의 사각형에 걸침
+    # 2. 사각형 내부의 점은 4개의 사각형에 걸침
+num = allPoint - sidePoint * 4 - (sidePoint**2*3)
+print(num)
