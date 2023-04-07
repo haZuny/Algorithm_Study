03a70f16a9344b6bf693376ad2ffeafe8b7c7bb5
haZuny <gkwns5791@naver.com>
2023-04-07 15:10:18 +0900

    24267

--- 단계별로 풀어보기/24267 알고리즘 수업 - 알고리즘의 수행 시간 6.py
+++ 단계별로 풀어보기/24267 알고리즘 수업 - 알고리즘의 수행 시간 6.py
@@ -6,10 +6,18 @@ MenOfPassion(A[], n) {
             for k <- j + 1 to n
                 sum <- sum + A[i] × A[j] × A[k]; # 코드1
     return sum;
+
+    case 7
+    1 2 3 4 5
+      2 3 4 5 6
+        3 4 5 6 7
+
+        7 6 5
+        3 2 1
 '''
 
 import sys
 
 n = int(sys.stdin.readline())
-print(n * (n+1) * (n-4) // 6 + n)    # 코드 1 수행 횟수
+print(n * (n-1) * (n-2) // 6)    # 코드 1 수행 횟수
 print(3)    # 코드 1 O(n)
