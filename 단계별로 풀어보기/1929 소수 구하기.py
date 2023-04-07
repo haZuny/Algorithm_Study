686f2706073aa13cb970a93e954f8e33bb45b56f
haZuny <gkwns5791@naver.com>
2023-04-08 02:09:34 +0900

    실패..

--- 단계별로 풀어보기/1929 소수 구하기.py
+++ 단계별로 풀어보기/1929 소수 구하기.py
@@ -1,16 +1,33 @@
+'''
+2 #### 3 5 7 ## 11 13 ## 17 19 ## 23 ## ## 29 31 33 ## 37 39 41 43 ## 47 ## 51 53 ##
+
+
+마지막: 1 3 7 9
+'''
+
 import sys
 
-m, n = map(int, sys.stdin.readline().split())
+m,n = map(int, sys.stdin.readline().split())
 
+list = [i for i in range(3, n+1, 2)]
 
+if m <= 2:
+    print(2)
 
-for i in range(m, n+1):
-    flag = True
-    if i == 1:
-        flag = False
-    for j in range(2, i):
-        if i % j == 0:
-            flag = False
-            break
-    if(flag):
-        print(i)
+listSize = len(list)
+i = 0
+while i < len(list):
+    val = list[i]
+    if val >= m:
+        print(val)
+        
+    j = i + 1
+    while j < listSize:
+        if list[j] % val ==0:
+            list.pop(j)
+            listSize -= 1
+        else:
+            j += 1
+    
+    i += 1
+
