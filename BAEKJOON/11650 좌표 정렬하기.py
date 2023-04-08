b8cd7536c348273538be8e373e57269e07344e2e
haZuny <gkwns5791@naver.com>
2023-04-08 19:15:24 +0900

    11650

---
+++ BAEKJOON/11650 좌표 정렬하기.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(sys.stdin.readline())
+dic = {}
+for _ in range(n):
+    x, y = map(int, sys.stdin.readline().split())
+    if x in dic:
+        dic[x].append(y)
+    else:
+        dic[x] = [y]
+for x in sorted(dic):
+    ys = dic[x]
+    ys.sort()
+    for y in ys:
+        print(x,y)
