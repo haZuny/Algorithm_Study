a73f5649319d278d751e6b54bf7fe56136b796b9
haZuny <gkwns5791@naver.com>
2023-04-08 19:55:31 +0900

    18870

--- BAEKJOON/18870 좌표 압축.py
+++ BAEKJOON/18870 좌표 압축.py
@@ -1,14 +1,17 @@
 import sys
 
 n = int(sys.stdin.readline())
-pure = []
+dic = {}
 list = list(map(int, sys.stdin.readline().split()))
 
 for num in list:
-    if not num in pure:
-        pure.append(num)
-        
-pure.sort()
+    if not num in dic:
+        dic[num] = 0
+
+sortDic = sorted(dic)
+
+for i in range(len(sortDic)):
+    dic[sortDic[i]] = i
 
 for num in list:
-    print(pure.index(num), end=' ')
+    print(dic[num], end=' ')
