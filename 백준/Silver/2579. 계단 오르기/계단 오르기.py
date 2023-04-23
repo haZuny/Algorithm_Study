4073473c33e9ef08acecbec9b5f6c7c2c4512ab4
Hajun Kwon <64102831+haZuny@users.noreply.github.com>
2023-04-23 22:58:53 +0900

    [Silver III] Title: 계단 오르기, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -2,13 +2,13 @@ n = int(input())
 lst = [(int(input()),)]    #[(한칸오른경우, 두칸오른경우)]
 for _ in range(n-1):
     n = int(input())
-    try:    # 두번째 계단
+    try:
         ifSeq = lst[-1][1] + n
-    except:
+    except:    # 두번째 계단 에러 방지
         ifSeq = lst[-1][0] + n
-    try:    # 두번째 계단
+    try:
         ifNotSeq = max(lst[-2]) + n
-    except:
+    except:    # 두번째 계단 에러 방지
         ifNotSeq = n
     lst.append((ifSeq, ifNotSeq))
 print(max(lst[-1]))
