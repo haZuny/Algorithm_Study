8f15a7737b63a8b3d497eb07bc103cf2dbea48f0
Hajun Kwon <64102831+haZuny@users.noreply.github.com>
2023-04-30 23:22:08 +0900

    [Gold V] Title: 전깃줄, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.py
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -1,6 +1,6 @@
 '''
 1. 1차원 리스트 형태로 전깃줄 모양 저장
-2. 모든 부분이 증가 혹은 감소 수열이여야지 안꼬임
+2. 모든 부분이 증가 수열이여야지 안꼬임
 3. 최대 증가 수열 길이 구함
 4. 전체 길이에서 마이너스
 '''
@@ -9,8 +9,7 @@ n = int(input())
 
 temp = [-1 for _ in range(500)]
 arr = []
-dp_inc = [1 for _ in range(n)]    # 증가 수열
-dp_dec = [1 for _ in range(n)]    # 감소 수열
+dp = [1 for _ in range(n)]    # 증가 수열
 
 # 전기줄 순서대로 리스트에 저장
 for i in range(n):
@@ -20,12 +19,10 @@ for i in temp:
     if i != -1:
         arr.append(i)
 
+# dp 탐색
 for i in range(n):
     for j in range(i):
         if arr[i] > arr[j]:
-            dp_inc[i] = max(dp_inc[i], dp_inc[j]+1)
-        else:
-            dp_dec[i] = max(dp_dec[i], dp_dec[j]+1)
-            
-#print(n - max(dp_inc + dp_dec))
-print(n-max(dp_inc))
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(n-max(dp))
