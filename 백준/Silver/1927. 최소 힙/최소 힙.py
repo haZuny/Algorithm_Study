3123444ee2018d538791cc9c774a5af0dbdbfbc3
Hajun Kwon <64102831+haZuny@users.noreply.github.com>
2023-05-30 16:53:22 +0900

    [Silver II] Title: 최소 힙, Time: 176 ms, Memory: 37180 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,14 @@
+import heapq, sys
+
+heap = []
+
+n = int(input())
+for _ in range(n):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        try:
+            print(heapq.heappop(heap))
+        except:
+            print(0)
+    else:
+        heapq.heappush(heap, x)
