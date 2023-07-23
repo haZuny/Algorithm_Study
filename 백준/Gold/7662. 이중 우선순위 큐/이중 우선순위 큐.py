4dea895c74a8321d14abc67dfda64daa33f0024a
Hajun Kwon <64102831+haZuny@users.noreply.github.com>
2023-07-23 21:15:21 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 4292 ms, Memory: 243680 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -1,88 +1,100 @@
+'''
+1 힙 두개를 사용해서 풀이
+2 push연산에서 min heap, max heap 두개 모두 각각 삽입
+3 push된 num은 nums에 기록
+4 pop 연산시 두개 힙에서 각각 pop
+5 만약 두 힙에 각각 값이 하나씩 남으면 dict에서 출력
+'''
 import collections, heapq, sys
 
+nums = {}
+cnt = 0
+maxHeap = []
+minHeap = []
 
+# delete dict
+def deleteNum(n):
+    global nums
+
+    nums[n] -= 1
+    if nums[n] == 0:
+        nums.pop(n)
+    
+# heap push
+def heapPush(n):
+    global cnt
+    
+    heapq.heappush(maxHeap, -1 * n)
+    heapq.heappush(minHeap, n)
+
+    try:
+        nums[n] += 1
+    except:
+        nums[n] = 1
+    cnt += 1
+    
+
+# heap pop
+def heapPop(isMax):
+    global cnt
+
+    if cnt == 0:
+        return None
+    
+    elif cnt == 1:
+        while True:
+            v = heapq.heappop(minHeap)
+            if v in nums:
+                break
+        cnt -= 1
+        deleteNum(v)
+        return v
+            
+    else:
+        if isMax:
+            while True:
+                v = -1 * heapq.heappop(maxHeap)
+                if v in nums:
+                    break
+        else:
+            while True:
+                v = heapq.heappop(minHeap)
+                if v in nums:
+                    break
+        cnt -= 1
+        deleteNum(v)
+        return v
+        
 t = int(sys.stdin.readline())
 
 for _ in range(t):
     # 입력 및 초기화
     q = int(sys.stdin.readline())
 
+    nums = {}
     cnt = 0
     maxHeap = []
     minHeap = []
-    id = 0
-    nums = set()
     
     for _ in range(q):
         command, n = input().split()
         n = int(n)
-        
-        # push
         if command == 'I':
-            heapq.heappush(maxHeap, (-n, id))
-            heapq.heappush(minHeap, (n, id))
-            nums.add(id)
-            id +=1; cnt+= 1
-
-        # pop max
+            heapPush(n)
         elif n == 1:
-            v = None
-            while maxHeap:
-                v, k = heapq.heappop(maxHeap)
-                v *= -1
-                if k in nums:
-                    break
-                else:
-                    v = None
-            if v != None:
-                cnt -= 1
-                nums.remove(k)
-
-        # pop min
+            heapPop(True)
         else:
-            v = None
-            while minHeap:
-                v, k = heapq.heappop(minHeap)
-                if k in nums:
-                    break
-                else:
-                    v = None
-            if v != None:
-                cnt -= 1
-                nums.remove(k)
+            heapPop(False)
 
-    # 출력1
-    maxv = None
-    while maxHeap:
-        maxv, k = heapq.heappop(maxHeap)
-        maxv *= -1
-        if k in nums:
-            break
-        else:
-            maxv = None
-    if maxv != None:
-        cnt -= 1
-        nums.remove(k)
 
-    # 출력2
-    minv = None
-    while minHeap:
-        minv, k = heapq.heappop(minHeap)
-        if k in nums:
-            break
-        else:
-            minv = None
-    if minv != None:
-        cnt -= 1
-        nums.remove(k)
-
-    # 0개, 1개 남은 경우 처리, 출력
-    if maxv == None:
+    maxV = heapPop(True)
+    minV = heapPop(False)
+    
+    if maxV == None:
         print('EMPTY')
     else:
-        if minv == None:
-            minv = maxv
-        print(maxv, minv)
-
-
+        if minV == None:
+            minV = maxV
+        print(maxV, minV)
+    
 
