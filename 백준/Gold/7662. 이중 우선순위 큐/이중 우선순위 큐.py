1a1ef04a67ec0fd2795b536a124c59bbacd7878a
Hajun Kwon <64102831+haZuny@users.noreply.github.com>
2023-07-23 21:10:31 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 6108 ms, Memory: 415248 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -1,27 +1,88 @@
-import sys;read=sys.stdin.readline
-import heapq
+import collections, heapq, sys
 
-result=[]
-for T in range(int(read())):
-    visited=[False]*1_000_001
-    minH,maxH=[],[]
-    for i in range(int(read())):
-        s=read().split()
-        if s[0]=='I':
-            heapq.heappush(minH,(int(s[1]),i))
-            heapq.heappush(maxH,(-int(s[1]),i))
-            visited[i]=True
-        elif s[1]=='1':
-            while maxH and not visited[maxH[0][1]]:heapq.heappop(maxH)
-            if maxH:
-                visited[maxH[0][1]]=False
-                heapq.heappop(maxH)
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    # 입력 및 초기화
+    q = int(sys.stdin.readline())
+
+    cnt = 0
+    maxHeap = []
+    minHeap = []
+    id = 0
+    nums = set()
+    
+    for _ in range(q):
+        command, n = input().split()
+        n = int(n)
+        
+        # push
+        if command == 'I':
+            heapq.heappush(maxHeap, (-n, id))
+            heapq.heappush(minHeap, (n, id))
+            nums.add(id)
+            id +=1; cnt+= 1
+
+        # pop max
+        elif n == 1:
+            v = None
+            while maxHeap:
+                v, k = heapq.heappop(maxHeap)
+                v *= -1
+                if k in nums:
+                    break
+                else:
+                    v = None
+            if v != None:
+                cnt -= 1
+                nums.remove(k)
+
+        # pop min
         else:
-            while minH and not visited[minH[0][1]]:heapq.heappop(minH)
-            if minH:
-                visited[minH[0][1]]=False
-                heapq.heappop(minH)
-    while minH and not visited[minH[0][1]]:heapq.heappop(minH)
-    while maxH and not visited[maxH[0][1]]:heapq.heappop(maxH)
-    result.append(f'{-maxH[0][0]} {minH[0][0]}'if maxH and minH else'EMPTY')
-print('\n'.join(result))
+            v = None
+            while minHeap:
+                v, k = heapq.heappop(minHeap)
+                if k in nums:
+                    break
+                else:
+                    v = None
+            if v != None:
+                cnt -= 1
+                nums.remove(k)
+
+    # 출력1
+    maxv = None
+    while maxHeap:
+        maxv, k = heapq.heappop(maxHeap)
+        maxv *= -1
+        if k in nums:
+            break
+        else:
+            maxv = None
+    if maxv != None:
+        cnt -= 1
+        nums.remove(k)
+
+    # 출력2
+    minv = None
+    while minHeap:
+        minv, k = heapq.heappop(minHeap)
+        if k in nums:
+            break
+        else:
+            minv = None
+    if minv != None:
+        cnt -= 1
+        nums.remove(k)
+
+    # 0개, 1개 남은 경우 처리, 출력
+    if maxv == None:
+        print('EMPTY')
+    else:
+        if minv == None:
+            minv = maxv
+        print(maxv, minv)
+
+
+
