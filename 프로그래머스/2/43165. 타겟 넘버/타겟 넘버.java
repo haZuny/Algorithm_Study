a182e8f6b0c812d1b8fe4158c674d9b30922a48c
Hajun Kwon <64102831+haZuny@users.noreply.github.com>
2024-04-09 23:45:51 +0900

    [level 2] Title: 타겟 넘버, Time: 0.38 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+
+
+class Solution {
+    int answer = 0;
+    int[] numbers;
+    int target;
+    
+    public int solution(int[] numbers, int target) {
+        this.numbers = numbers;
+        this.target = target;
+        
+        reculsive(0,0);
+        return answer;
+    }
+    
+    void reculsive(int sum, int idx){
+        if (idx == numbers.length && sum == target)  answer++;
+        else if(idx == numbers.length)   return;
+        else{
+            reculsive(sum+numbers[idx], idx+1);
+            reculsive(sum-numbers[idx], idx+1);
+        }
+    }
+}
